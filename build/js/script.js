37d327badc0a82d062f8898391f31181ffb70e5c
kevin344k <kevin344k@gmail.com>
2023-12-09 10:20:26 -0500

    se mejora el modal warning y el line height en firefox

--- build/js/script.js
+++ build/js/script.js
@@ -1,2 +1,22 @@
-//modal header
 "use strict";
+
+//modal 
+var ventanaModal = document.querySelector("#ventanaModal");
+var btnCerrarModal = document.querySelector("#btnCerrarModal");
+var botonModal = document.querySelector(".boton-modal");
+window.addEventListener("load", function () {
+  ventanaModal.style.display = "block";
+  return false;
+});
+btnCerrarModal.addEventListener("click", function () {
+  ventanaModal.style.display = "none";
+  return false;
+});
+btnCerrarModal.addEventListener("click", function () {
+  ventanaModal.style.display = "none";
+  return false;
+});
+botonModal.addEventListener("click", function () {
+  ventanaModal.style.display = "none";
+  return false;
+});
