14f12990252d7c3d94b74769874d91ed76036558
kevin344k <kevin344k@gmail.com>
2024-05-29 22:54:25 -0500

    se corrije el error de -clock is not defined-

--- src/clockHalvin/main.js
+++ src/clockHalvin/main.js
@@ -7,19 +7,25 @@ const init = async () => {
     const blocksTipHeight = await blocks.getBlocksTipHeight();
    let resultHeightBlock= JSON.stringify(blocksTipHeight, undefined, 2);
    console.log(resultHeightBlock);
-const blockText=document.querySelector('.text-rewards-halvin')
+
 //blockText.textContent= ` Bloque ${resultHeightBlock} de 840.000  ` 
 let CurrentDate=Date.now() //fecha actual
 let timeToHalvin=((1050000 - resultHeightBlock)*(10))*60
 let dateH=CurrentDate+timeToHalvin*1000
 console.log(dateH);
+
 $(document).ready(function(){ 
-    clock=$('.clock').FlipClock(timeToHalvin,{
+  var   clock=$('.clock').FlipClock(timeToHalvin,{
         clockFace: 'DailyCounter',
         autoStart:true,
         countdown:true
         }
+
 )})
   };
+
+
+
+
   init();
 
