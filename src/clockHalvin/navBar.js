efb42ff5e1f9b5b5a07b4fc621eb114d835d5bf6
kevin344k <kevin344k@gmail.com>
2025-04-23 10:20:00 -0500

    se concluye es slider y footer navbar e indicador fear and greed

---
+++ src/clockHalvin/navBar.js
@@ -0,0 +1,30 @@
+// Obtener elementos del DOM
+const menuToggle = document.getElementById("menuToggle");
+const sideMenu = document.getElementById("sideMenu");
+const closeMenu = document.getElementById("closeMenu");
+const overlay = document.getElementById("overlay");
+
+// Función para abrir el menú
+menuToggle.addEventListener("click", () => {
+    console.log('click abrir menu');
+    
+  sideMenu.classList.add("active");
+  overlay.classList.add("active");
+});
+
+// Función para cerrar el menú
+closeMenu.addEventListener("click", () => {
+  sideMenu.classList.remove("active");
+  overlay.classList.remove("active");
+});
+// Detectar clics fuera del menú
+document.addEventListener("click", (event) => {
+  const isClickInsideMenu = sideMenu.contains(event.target);
+  const isClickOnToggle = menuToggle.contains(event.target);
+
+  // Si el clic no está dentro del menú ni en el botón de apertura, cerrar el menú
+  if (!isClickInsideMenu && !isClickOnToggle) {
+    sideMenu.classList.remove("active");
+    overlay.classList.remove("active");
+  }
+});
--- src/clockHalvin/sliderTestimonios.js
+++ src/clockHalvin/sliderTestimonios.js
@@ -1,85 +1,71 @@
-const sliderContainer = document.querySelector('.slider-container')
-const arr = document.querySelectorAll('.image')
+const sliderContainer = document.querySelector(".slider-container");
+const arr = document.querySelectorAll(".image");
 
-const arrow_left = document.querySelector('#arrow_left')
-const arrow_right = document.querySelector('#arrow_right')
+const arrow_left = document.querySelector("#arrow_left");
+const arrow_right = document.querySelector("#arrow_right");
 
-
-const parent_indicator = document.querySelector('.indicator-slider')
-
-
-
+const parent_indicator = document.querySelector(".indicator-slider");
 
 for (let i = 0; i < arr.length; i++) {
-    const element = document.createElement('span')
-    element.classList.add('indicator')
-    parent_indicator.appendChild(element)
-
+  const element = document.createElement("span");
+  element.classList.add("indicator");
+  parent_indicator.appendChild(element);
 }
 
-const indicators_group = document.querySelectorAll('.indicator')
-
-indicators_group[3].classList.add('activate_indicator')
+const indicators_group = document.querySelectorAll(".indicator");
 
 
-let sliderPosition = 0
-let indexArr = 0
 
-if (window.innerWidth>=1024) {
-    indexArr = 3
-    console.log('pantalla grande');
-    
+let sliderPosition = 0; //oindicador redondo de posicion de la imgen visible vs las que faltan
+let indexArr = 0;
+
+if (window.innerWidth >= 768 && window.innerWidth < 1024) {
+  indexArr = 1;
+  console.log("pantalla mediana");
+}
+if (window.innerWidth >= 1024) {
+  indexArr = 3;
+  console.log("pantalla grande");
 }
 
+indicators_group[indexArr].classList.add("activate_indicator");
 //para dispositivos pequeños el width de cada imagen debe ser de 320px
 
+arrow_right.addEventListener("click", () => {
+  console.log(indexArr < arr.length);
 
+  if (indexArr < arr.length - 1) {
+    sliderContainer.style.left = `${sliderPosition - 320}px`;
+    indicators_group[indexArr].classList.remove("activate_indicator");
+    indicators_group[indexArr + 1].classList.add("activate_indicator");
+    sliderPosition = sliderPosition - 320;
+    indexArr++;
+    // console.log(indexArr);
+  } else {
+    console.log("no se puede ir mas allá");
+  }
+});
 
+arrow_left.addEventListener("click", () => {
+  //  console.log(indexArr);
+  let positinoLeftArr = 0;
 
+  if (window.innerWidth >= 768 && window.innerWidth < 1024) {
+    positinoLeftArr = 2;
+    console.log("pantalla mediana");
+  }
+  if (window.innerWidth >= 1024) {
+    positinoLeftArr = 4;
+    console.log("pantalla grande");
+  }
+console.log(indexArr, positinoLeftArr);
 
-arrow_right.addEventListener('click', () => {
-
-
-    console.log(indexArr < arr.length);
-
-    if (indexArr < arr.length - 1) {
-
-        sliderContainer.style.left = `${sliderPosition - 320}px`
-        indicators_group[indexArr].classList.remove('activate_indicator')
-        indicators_group[indexArr + 1].classList.add('activate_indicator')
-        sliderPosition = sliderPosition - 320
-        indexArr++
-        console.log(indexArr);
-    } else {
-        console.log('no se puede ir mas allá');
-
-    }
-
-
-})
-
-arrow_left.addEventListener('click', () => {
-
-
-    console.log(indexArr);
-
-    let positinoLeftArr=window.innerWidth>=1024? 4: 0  //esto p3ermite q el slider se detenga en la imagen 3
-
-    if (indexArr >= positinoLeftArr) {
-        sliderContainer.style.left = `${sliderPosition + 320}px`
-        indicators_group[indexArr].classList.remove('activate_indicator')
-        indicators_group[indexArr - 1].classList.add('activate_indicator')
-        sliderPosition = sliderPosition + 320
-        console.log(indexArr);
-        indexArr--
-
-    }
-
-
-
-})
-
-
-
-
-
+  if (indexArr > positinoLeftArr) {
+    sliderContainer.style.left = `${sliderPosition + 320}px`;
+    indicators_group[indexArr].classList.remove("activate_indicator");
+    indicators_group[indexArr - 1].classList.add("activate_indicator");
+    sliderPosition = sliderPosition + 320;
+    //   console.log(indexArr);
+    indexArr--;
+  }
+});
