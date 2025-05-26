387f41d0f4ae7842d0728ad6f61d9b16e236168a
kevin344k <kevin344k@gmail.com>
2025-05-25 21:46:12 -0500

    sdfsdfsd

--- src/clockHalvin/modal_antiscam.js
+++ src/clockHalvin/modal_antiscam.js
@@ -4,7 +4,7 @@ const modalAlertScam = document.getElementById("modalAlertScam");
 const close_alert_scam=document.querySelector('.close-alert-scam')
 const boton_modal_antiscam=document.querySelector('.boton-modal-antiscam')
 // Get the button that opens the modal
-
+/*
 // When the user clicks on <span> (x), close the modal
 close_alert_scam.onclick = function () {
   modalAlertScam.style.display = "none";
@@ -26,7 +26,7 @@ window.onclick = function (event) {
   if (event.target == modal) {
     modalAlertScam.style.display = "none";
   }
-};
+};*/
 //esto detecta el scroll hacia abajo y hace aparecer el boton de scroll to top
 window.addEventListener('scroll', () => {
   const btn = document.querySelector('.but-floating');
